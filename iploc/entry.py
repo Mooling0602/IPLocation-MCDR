c0e5056be96149bafbc3d14945552a4e02336ae6
木泠 <clemooling@outlook.com>
2024-11-09 11:35:25 +0000

    feat MatrixSync-v2.4.0 API change

--- iploc/entry.py
+++ iploc/entry.py
@@ -11,6 +11,10 @@ try:
 except ModuleNotFoundError:
     # 没有检测到MatrixSync，仅进行广播
     send = lambda *args, **kwargs: psi.broadcast(*args, **kwargs)
+except ImportError:
+    # 无法导入MatrixSync的API，判断为2.4.0新版本，导入修改后的新接口名称
+    from matrix_sync.reporter import send_matrix # type: ignore
+    send = lambda *args, **kwargs: (send_matrix(*args, **kwargs), psi.broadcast(*args, **kwargs))
 
 def on_load(server: PluginServerInterface, old):
     server.register_event_listener('player_ip_logger.player_login', on_player_ip_logged)
@@ -29,16 +33,3 @@ def on_player_ip_logged(server: PluginServerInterface, player_name:str, player_i
         send(f"[!] 玩家 {player} 的IP归属地：{location}")
     except ModuleNotFoundError:
         server.logger.error(f"不支持该API接口: {using_api}，请正确配置插件（使用taobao或baidu）！")
-
-# 旧版检测方式，在玩家上线时解析其IP，若上面调用的插件工作稳定，此部分将在后续版本彻底移除
-# def on_player_joined(server: PluginServerInterface, player: str, info: Info):
-#     server.logger.info(f"正在查询玩家{player}的IP归属地...")
-#     ip_pattern = r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
-#     match = re.search(ip_pattern, info.content)
-#     if match:
-#         ip = match.group(0)  ## 提取出IP地址
-#         using_api = iploc.config.api
-#         iploc = getattr(iploc.api, using_api)(ip)
-#         send(f"[!] 玩家 {player} 的IP归属地：{iploc}")
-#     else:
-#         send(f"[!] 无法提取玩家 {player} 的IP地址")

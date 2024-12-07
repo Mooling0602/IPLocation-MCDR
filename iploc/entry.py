9b171e8c44e28ca20401a210ef1b73b992356193
Mooling0602 <clemooling@outlook.com>
2024-12-07 23:37:59 +0800

    修改源代码中部分注释内容，无实际更新

--- iploc/entry.py
+++ iploc/entry.py
@@ -1,19 +1,17 @@
-import re
-
 from importlib import import_module
 from mcdreforged.api.all import *
 from iploc.config import config
 
 psi = ServerInterface.psi()
 try:
-    from matrix_sync.reporter import send_matrix
-    # 同时转发到Matrix，若检测到插件MatrixSync正常工作，其v2.3.x版本支持已废弃，具体见其Release更新日志
+    # MatrixSync v2.4.0+
+    from matrix_sync.reporter import send_matrix # type: ignore
     send = lambda *args, **kwargs: (send_matrix(*args, **kwargs), psi.broadcast(*args, **kwargs))
 except ModuleNotFoundError:
     # 没有检测到MatrixSync，仅进行广播
     send = lambda *args, **kwargs: psi.broadcast(*args, **kwargs)
 
-def on_load(server: PluginServerInterface, old):
+def on_load(server: PluginServerInterface, prev_module):
     server.register_event_listener('player_ip_logger.player_login', on_player_ip_logged)
 
 
